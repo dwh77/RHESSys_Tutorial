7e1bf23dc28220db420448542d6e1a238b4add2f
dwh77 <dwh1998@vt.edu>
2026-04-30 17:15:48 -0400

    add rhessys output

--- CCR_AR_modeling/Scripts/Data_Comp_AR.R
+++ CCR_AR_modeling/Scripts/Data_Comp_AR.R
@@ -60,19 +60,80 @@ AR_df <- full_join(daily_met, catwalk_daily, by = "Date")
 # write.csv(AR_df, "./CCR_AR_Modeling/Data/Daily_catwalk_met_2021_2025.csv", row.names = F)
 
 #### Make lags and ZT data ----
+# AR_df <- read_csv("./CCR_AR_Modeling/Data/Daily_catwalk_met_2021_2025.csv")
+#
+# AR_df_lag <- AR_df |>
+#   mutate(fDOM_1m_lag1 = lag(fDOM_1_QSU_daily , 1),
+#          fDOM_9m_lag1 = lag(fDOM_9_QSU_daily , 1),
+#          Rain_mm_lag1 = lag(Rain_mm_daily, 1),
+#          Rain_mm_lag2 = lag(Rain_mm_daily, 2)) |>
+#   mutate(
+#     Rain_mm_10day    = slider::slide_dbl(Rain_mm_daily,    sum,  .before = 10, .after = 0, .complete = TRUE),
+#     Chla_1_ugL_10day = slider::slide_dbl(Chla_1_ugL_daily, mean, .before = 10, .after = 0, .complete = F)
+#   )
+#
+# AR_df_lag_ZS <- AR_df_lag |>
+#   mutate(across(
+#     .cols = !Date,
+#     .fns  = ~ scale(.x)[,1],
+#     .names = "{.col}_ZS"
+#   ))
+
+
+# write.csv(AR_df_lag_ZS, "./CCR_AR_Modeling/Data/Daily_catwalk_met_2021_2025_lagZS.csv", row.names = F)
+
+
+#### Get RHESSys data and format to bind ----
+## read in rest of daily
 AR_df <- read_csv("./CCR_AR_Modeling/Data/Daily_catwalk_met_2021_2025.csv")
 
-AR_df_lag <- AR_df |>
-  mutate(fDOM_1m_lag1 = lag(fDOM_1_QSU_daily , 1),
-         fDOM_9m_lag1 = lag(fDOM_9_QSU_daily , 1),
-         Rain_mm_lag1 = lag(Rain_mm_daily, 1),
-         Rain_mm_lag2 = lag(Rain_mm_daily, 2)) |>
-  mutate(
-    Rain_mm_10day    = slider::slide_dbl(Rain_mm_daily,    sum,  .before = 10, .after = 0, .complete = TRUE),
-    Chla_1_ugL_10day = slider::slide_dbl(Chla_1_ugL_daily, mean, .before = 10, .after = 0, .complete = F)
-  )
+##read in RHESSys
+workpath <- "C:/Users/dwh18/OneDrive/Desktop/R_Projects/RHESSys_development/ccr_rhessys/out/ccr_patch500"
 
-AR_df_lag_ZS <- AR_df_lag |>
+output_grow <- read_delim(paste0(workpath, "/harvest1850_2025run_grow_basin.daily"),
+                          delim = " ", col_names = T)
+
+output_h2o <- read_delim(paste0(workpath, "/harvest1850_2025run_basin.daily"),
+                         delim = " ", col_names = T)
+
+# HPB area is 4.299446 km^2
+# hpb_area_m2 <- 4.299446 * 1000000
+ccr_area_m2 <- 45.83578 * 1000000
+
+
+output_h2o_grow <- left_join(output_h2o, output_grow, by = c("day", "month", "year", "basinID")) |>
+  mutate(date = ymd(paste(year, month, day, sep = "-"))) |>
+  filter(date >= ymd("2020-01-01")) |>
+  select(date, streamflow, streamflow_NO3, streamflow_DOC, lai.y) |>
+  rename(lai = lai.y) |>
+  #Q unit conversions
+  mutate(streamflow_m_day = streamflow / 1000) |> #convert mm/day to m/day
+  mutate(streamflow_m3_day = streamflow_m_day * ccr_area_m2) |> #convert m/day to m3/day
+  #chem conversions from g/m2/day to mg/L
+  mutate(DOC_mgL = streamflow_DOC / streamflow_m_day,
+         NO3_mgL = streamflow_NO3 / streamflow_m_day)
+
+
+rhessys_AR <- output_h2o_grow |>
+  rename(Date = date) |>
+  select(Date, streamflow_DOC, streamflow_m3_day, DOC_mgL) |>
+  rename(RH_streamflow_DOC_gm2day = streamflow_DOC,
+         RH_Q_m3day = streamflow_m3_day,
+         RH_DOC_mgL = DOC_mgL)
+
+
+
+#### Z score and export
+AR_df_lag_ZS <- AR_df |>
+    mutate(fDOM_1m_lag1 = lag(fDOM_1_QSU_daily , 1),
+           fDOM_9m_lag1 = lag(fDOM_9_QSU_daily , 1),
+           Rain_mm_lag1 = lag(Rain_mm_daily, 1),
+           Rain_mm_lag2 = lag(Rain_mm_daily, 2)) |>
+    mutate(
+      Rain_mm_10day    = slider::slide_dbl(Rain_mm_daily,    sum,  .before = 10, .after = 0, .complete = TRUE),
+      Chla_1_ugL_10day = slider::slide_dbl(Chla_1_ugL_daily, mean, .before = 10, .after = 0, .complete = F)
+    ) |>
+  left_join(rhessys_AR, by = "Date") |>
   mutate(across(
     .cols = !Date,
     .fns  = ~ scale(.x)[,1],
@@ -80,5 +141,6 @@ AR_df_lag_ZS <- AR_df_lag |>
   ))
 
 
-# write.csv(AR_df_lag_ZS, "./CCR_AR_Modeling/Data/Daily_catwalk_met_2021_2025_lagZS.csv", row.names = F)
+ # write.csv(AR_df_lag_ZS, "./CCR_AR_Modeling/Data/Daily_catwalk_met_RH_2021_2025_lagZS.csv", row.names = F)
+
 
